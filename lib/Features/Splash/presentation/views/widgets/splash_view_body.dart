b00d48d77bc8334bcae28ec60de24f22686a8181
BeshoyMarkElMallah <bisho.king.980@gmail.com>
2023-08-12 20:44:55 +0300

    add sliding animation in splash screen

--- lib/Features/Splash/presentation/views/widgets/splash_view_body.dart
+++ lib/Features/Splash/presentation/views/widgets/splash_view_body.dart
@@ -1,8 +1,41 @@
+import 'package:bookly_bloc/Features/Splash/presentation/views/widgets/sliding_text.dart';
 import 'package:bookly_bloc/core/utils/assets.dart';
 import 'package:flutter/cupertino.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({Key? key}) : super(key: key);
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 10),
+    );
+
+    slidingAnimation =
+        Tween<Offset>(begin: const Offset(0, 5), end: Offset.zero)
+            .animate(animationController);
+    animationController.forward();
+    slidingAnimation.addListener(() {
+      setState(() {});
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    animationController.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,16 +44,16 @@ class SplashViewBody extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
         Container(
-          padding: const EdgeInsets.all(50),
+          padding: const EdgeInsets.symmetric(horizontal: 50),
           child: Image.asset(
             AssetsData.logo,
             fit: BoxFit.fitHeight,
           ),
         ),
-        const Text(
-          "Read Free Books",
-          textAlign: TextAlign.center,
+        SizedBox(
+          height: 10,
         ),
+        SlidingText(slidingAnimation: slidingAnimation),
       ],
     );
   }
