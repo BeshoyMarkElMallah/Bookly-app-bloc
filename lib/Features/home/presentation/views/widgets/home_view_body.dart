173e0bc6cf82542c2f20a0a1b943f4906de39caf
BeshoyMarkElMallah <bisho.king.980@gmail.com>
2023-08-12 23:15:43 +0300

    add best seller item

--- lib/Features/home/presentation/views/widgets/home_view_body.dart
+++ lib/Features/home/presentation/views/widgets/home_view_body.dart
@@ -1,6 +1,6 @@
+import 'package:bookly_bloc/Features/home/presentation/views/widgets/best_seller_listview_item.dart';
 import 'package:bookly_bloc/Features/home/presentation/views/widgets/custom_appbar.dart';
 import 'package:bookly_bloc/Features/home/presentation/views/widgets/featured_book_listview.dart';
-import 'package:bookly_bloc/core/utils/assets.dart';
 import 'package:bookly_bloc/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 
@@ -23,34 +23,10 @@ class HomeViewBody extends StatelessWidget {
             'Best Seller',
             style: Styles.textStyle18,
           ),
-          BestSellerListViewItem(),
-        ],
-      ),
-    );
-  }
-}
-
-class BestSellerListViewItem extends StatelessWidget {
-  const BestSellerListViewItem({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: 125,
-      child: Row(
-        children: [
-          AspectRatio(
-            aspectRatio: 2.5 / 4,
-            child: Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(8),
-                color: Colors.red,
-                image: const DecorationImage(
-                    image: AssetImage(AssetsData.jungleBook), fit: BoxFit.fill),
-              ),
-            ),
+          SizedBox(
+            height: 20,
           ),
-          const Column(),
+          BestSellerListViewItem(),
         ],
       ),
     );
