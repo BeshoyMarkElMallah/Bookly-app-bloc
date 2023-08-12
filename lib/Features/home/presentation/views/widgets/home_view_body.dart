2cbce15c3bc8cf16a5da027c9d9683f157ca9c10
BeshoyMarkElMallah <bisho.king.980@gmail.com>
2023-08-12 23:38:32 +0300

    best seller list view scroll done

--- lib/Features/home/presentation/views/widgets/home_view_body.dart
+++ lib/Features/home/presentation/views/widgets/home_view_body.dart
@@ -1,4 +1,4 @@
-import 'package:bookly_bloc/Features/home/presentation/views/widgets/best_seller_listview_item.dart';
+import 'package:bookly_bloc/Features/home/presentation/views/widgets/best_seller_listview.dart';
 import 'package:bookly_bloc/Features/home/presentation/views/widgets/custom_appbar.dart';
 import 'package:bookly_bloc/Features/home/presentation/views/widgets/featured_book_listview.dart';
 import 'package:bookly_bloc/core/utils/styles.dart';
@@ -9,26 +9,37 @@ class HomeViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.symmetric(horizontal: 8.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          CustomAppBar(),
-          FeaturedBookListView(),
-          SizedBox(
-            height: 50,
+    return const CustomScrollView(
+      slivers: [
+        SliverToBoxAdapter(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              CustomAppBar(),
+              FeaturedBookListView(),
+              SizedBox(
+                height: 50,
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 30),
+                child: Text(
+                  'Best Seller',
+                  style: Styles.textStyle18,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+            ],
           ),
-          Text(
-            'Best Seller',
-            style: Styles.textStyle18,
+        ),
+        SliverFillRemaining(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 30),
+            child: BestSellerListView(),
           ),
-          SizedBox(
-            height: 20,
-          ),
-          BestSellerListViewItem(),
-        ],
-      ),
+        )
+      ],
     );
   }
 }
