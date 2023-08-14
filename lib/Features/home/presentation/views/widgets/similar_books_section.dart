8e6a20f9c3da9e3c063b73ad1d6747ceb77af6dd
BeshoyMarkElMallah <bisho.king.980@gmail.com>
2023-08-14 23:32:04 +0300

    refactor book details view

---
+++ lib/Features/home/presentation/views/widgets/similar_books_section.dart
@@ -0,0 +1,26 @@
+import 'package:bookly_bloc/Features/home/presentation/views/widgets/similar_books_listview.dart';
+import 'package:bookly_bloc/core/utils/styles.dart';
+import 'package:flutter/material.dart';
+
+class SimilarBooksSection extends StatelessWidget {
+  const SimilarBooksSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Align(
+          alignment: Alignment.centerLeft,
+          child: Text(
+            'You can also like',
+            style: Styles.textStyle14.copyWith(fontWeight: FontWeight.w600),
+          ),
+        ),
+        const SizedBox(
+          height: 16,
+        ),
+        const SimilarBooksListView(),
+      ],
+    );
+  }
+}
