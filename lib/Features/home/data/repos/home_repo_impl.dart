764d1bc2e27392a2e3a0be82cce3a5ca5b25591e
BeshoyMarkElMallah <bisho.king.980@gmail.com>
2023-08-15 21:42:55 +0300

    create server failure from dio error

--- lib/Features/home/data/repos/home_repo_impl.dart
+++ lib/Features/home/data/repos/home_repo_impl.dart
@@ -3,6 +3,7 @@ import 'package:bookly_bloc/Features/home/data/repos/home_repo.dart';
 import 'package:bookly_bloc/core/errors/failures.dart';
 import 'package:bookly_bloc/core/utils/api_service.dart';
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
 
 class HomeRepoImpl extends HomeRepo {
   final ApiService apiService;
@@ -20,7 +21,10 @@ class HomeRepoImpl extends HomeRepo {
       }
       return right(books);
     } on Exception catch (e) {
-      return left(ServerFailure());
+      if (e is DioException) {
+        return left(ServerFailure.fromDioError(e));
+      }
+      return left(ServerFailure(errmessage: e.toString()));
     }
   }
 
--- lib/core/errors/failures.dart
+++ lib/core/errors/failures.dart
@@ -1,3 +1,52 @@
-abstract class Failure {}
+import 'package:dio/dio.dart';
 
-class ServerFailure extends Failure {}
+abstract class Failure {
+  final String errmessage;
+
+  Failure({required this.errmessage});
+}
+
+class ServerFailure extends Failure {
+  ServerFailure({required super.errmessage});
+
+  factory ServerFailure.fromDioError(DioException dioException) {
+    switch (dioException.type) {
+      case DioExceptionType.connectionTimeout:
+        return ServerFailure(errmessage: 'Connection timeout with ApiServer');
+      case DioExceptionType.receiveTimeout:
+        return ServerFailure(
+            errmessage: 'Receive timeout in connection with ApiServer');
+      case DioExceptionType.sendTimeout:
+        return ServerFailure(errmessage: "Connection timeout");
+      case DioExceptionType.badResponse:
+        return ServerFailure.fromResponse(
+            dioException.response!.statusCode!, dioException.response!.data);
+      case DioExceptionType.cancel:
+        return ServerFailure(errmessage: "Request cancelled");
+      case DioExceptionType.unknown:
+        if (dioException.message!.contains('SocketException')) {
+          return ServerFailure(errmessage: "No internet connection");
+        }
+        return ServerFailure(errmessage: "Unexpected Error, Please try again");
+      default:
+        return ServerFailure(errmessage: "Something went wrong");
+    }
+  }
+
+  factory ServerFailure.fromResponse(int statusCode, dynamic response) {
+    switch (statusCode) {
+      case 400:
+        return ServerFailure(errmessage: response['error']['message']);
+      case 401:
+        return ServerFailure(errmessage: response['error']['message']);
+      case 403:
+        return ServerFailure(errmessage: response['error']['message']);
+      case 404:
+        return ServerFailure(errmessage: response['error']['message']);
+      case 500:
+        return ServerFailure(errmessage: response['error']['message']);
+      default:
+        return ServerFailure(errmessage: "Something went wrong");
+    }
+  }
+}
