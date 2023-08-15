5e4ed240e3a0927a9c3b3f95cee4c50f92f21286
BeshoyMarkElMallah <bisho.king.980@gmail.com>
2023-08-15 22:23:12 +0300

    use get_it package and make singletons and setup service locator

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,17 @@
+import 'package:bookly_bloc/Features/home/data/repos/home_repo_impl.dart';
+import 'package:bookly_bloc/Features/home/presentation/manager/featured_books_cubit/featured_books_cubit.dart';
+import 'package:bookly_bloc/Features/home/presentation/manager/newest_books_cubit/newest_books_cubit.dart';
 import 'package:bookly_bloc/constants.dart';
+import 'package:bookly_bloc/core/utils/api_service.dart';
 import 'package:bookly_bloc/core/utils/app_router.dart';
+import 'package:bookly_bloc/core/utils/service_locator.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
+  setup();
   runApp(const BooklyApp());
 }
 
@@ -12,13 +20,28 @@ class BooklyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerConfig: AppRouter.router,
-      theme: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: kPrimaryColor,
-        textTheme: GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => FeaturedBooksCubit(
+            getIt.get<HomeRepoImpl>(),
+          ),
+        ),
+        BlocProvider(
+          create: (context) => NewestBooksCubit(
+            getIt.get<HomeRepoImpl>(),
+          ),
+        ),
+      ],
+      child: MaterialApp.router(
+        routerConfig: AppRouter.router,
+        theme: ThemeData.dark().copyWith(
+          scaffoldBackgroundColor: kPrimaryColor,
+          textTheme:
+              GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
+        ),
+        debugShowCheckedModeBanner: false,
       ),
-      debugShowCheckedModeBanner: false,
     );
   }
 }
